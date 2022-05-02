6a7ddd0df5ee738598adbf1c8125fd506d3c7ee6
Nguyễn Văn Nhật <104755768+vannhat006@users.noreply.github.com>
2022-05-02 17:06:52 +0700

    Add files via upload

---
+++ LT_HDT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lt_hdt;
+
+/**
+ *
+ * @author Admin
+ */
+public class LT_HDT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello nhat");
+    }
+    
+}
